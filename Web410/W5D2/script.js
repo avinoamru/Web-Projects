005145c30529868768bd08f1e8835578a6310b3a
Avinoam Ruach <ruachavinoam@gmail.com>
2020-01-15 08:50:04 -0800

    adding W5D2

--- Web410/W5D2/script.js
+++ Web410/W5D2/script.js
@@ -1,50 +1,91 @@
 var pizza = {
-    name: 'Pizza',
-    price: 14,
-    popularity: 'high'
+    Name: 'Pizza',
+    Price: 14,
+    Popularity: 'high'
 }
 
 var hamburger = {
-    name: 'Hamburger',
-    price: 8,
-    popularity: 'high'
+    Name: 'Hamburger',
+    Price: 8,
+    Popularity: 'high'
 }
 
 var sushi = {
-    name: 'Sushi',
-    price: 5,
-    popularity: 'high'
+    Name: 'Sushi',
+    Price: 5,
+    Popularity: 'high'
 }
 
 var donair = {
-    name: 'Donair',
-    price: 7,
-    popularity: 'medium'
+    Name: 'Donair',
+    Price: 7,
+    Popularity: 'medium'
 }
 
 var friedChicken = {
-    name: 'Fried chicken',
-    price: 20,
-    popularity: 'high'
+    Name: 'Fried chicken',
+    Price: 20,
+    Popularity: 'high'
 }
 
 var cereal = {
-    name: 'Cereal',
-    price: 3,
-    popularity: 'high'
+    Name: 'Cereal',
+    Price: 3,
+    Popularity: 'high'
 }
 
 
 var meals = [pizza, hamburger, sushi, donair, friedChicken, cereal];
 
-var values = [];
 
-for (var i = 0; i < meals.length; i++){
 
-    values = values + Object.values(meals[i]);
+function fromDatatoTable(dataBase) {
+    var tableCell = document.createElement('table');
+    tableCell.setAttribute('id', 'myTable');
+    document.getElementById('main-table').appendChild(tableCell);
+
+    var keys = Object.keys(dataBase[0]);
+    var row = document.createElement('tr');
+
+    for (key in keys) {
+            var headingCell = document.createElement('th');
+                var keyValue = document.createTextNode(keys[key]);
+                headingCell.appendChild(keyValue);
+        row.appendChild(headingCell);
+        document.getElementById('myTable').appendChild(row);
+    }
+
+    for (i in dataBase) {
+        var row = document.createElement('tr');
+        var line = dataBase[i];
+        Object.keys(line).map(e => {
+            var dataBaseNode = document.createElement('td');
+            var value = document.createTextNode(line[e]);
+            dataBaseNode.appendChild(value);
+            row.appendChild(dataBaseNode);
+            
+        })
+        document.getElementById('myTable').appendChild(row);
+    }
 
 }
 
+fromDatatoTable(meals);
 
 
-console.log(values)
+// event listener
+
+
+
+
+
+var td = document.getElementById('td');
+var myTable = document.getElementById('myTable');
+
+myTable.addEventListener("mouseover", function( event ) {   
+
+event.target.style.borderColor = 'blue';
+    
+    
+  
+  }, false);
